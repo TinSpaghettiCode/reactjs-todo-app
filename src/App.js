761ff9f2dac5d022e3fb224a1747d76fbf332f24
Tin <21521538@gm.uit.edu.vn>
2023-12-15 15:48:58 +0700

    Update: Fix comparing string

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState, useEffect } from 'react';
+import TodoList from './components/TodoList';
+import Textfield from '@atlaskit/textfield';
+import Button from '@atlaskit/button';
+import { v4 } from 'uuid';
+
+const TODO_APP_STORAGE_KEY = 'TODO_APP';
 
 function App() {
+  // state, props
+  const [todoList, setTodoList] = useState([]); // array
+  const [textInput, setTextInput] = useState(''); // string
+
+  // useEffect(() => {
+  //   if (localStorage.getItem(TODO_APP_STORAGE_KEY)) {
+  //     setTodoList(JSON.parse(localStorage.getItem(TODO_APP_STORAGE_KEY)));
+  //   }
+  // }, []);
+
+  // useEffect(() => {
+  //   localStorage.setItem(TODO_APP_STORAGE_KEY, JSON.stringify(todoList));
+  // }, [todoList]);
+
+  useEffect(() => {
+    console.log('🚀 ~ file: App.js:37 ~ App ~ todoList', todoList);
+  }, [todoList]);
+
+  const onTextInputChange = useCallback((e) => {
+    setTextInput(e.target.value);
+  }, []);
+
+  const removeAccents = (str) => {
+    return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  };
+
+  const onAddBtnClick = useCallback(
+    (e) => {
+      // them textInput vao danh sach todoList
+      const isTodoExist = todoList.some(
+        (todo) =>
+          removeAccents(todo.name).toLowerCase() ===
+          removeAccents(textInput).toLowerCase()
+      );
+      if (isTodoExist) {
+        // Nếu công việc đã tồn tại, hiển thị thông báo cho người dùng
+        alert('Công việc này đã tồn tại trong danh sách.');
+      } else {
+        // Nếu công việc chưa tồn tại, thêm nó vào danh sách
+        setTodoList([
+          { id: v4(), name: textInput, isCompleted: false },
+          ...todoList,
+        ]);
+
+        setTextInput('');
+      }
+
+      console.log('🚀 ~ file: App.js:27 ~ App ~ todoList:', todoList);
+    },
+
+    [textInput, todoList]
+  );
+
+  const onCheckBtnClick = useCallback((id) => {
+    setTodoList((prev) =>
+      prev.map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: true } : todo
+      )
+    );
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h3>Danh sách các việc cần làm</h3>
+      <Textfield
+        name="add-todo"
+        placeholder="Thêm việc cần làm..."
+        elemAfterInput={
+          <Button
+            isDisabled={!textInput}
+            appearance="primary"
+            onClick={onAddBtnClick}
+            style={{ marginRight: '2px' }}
+          >
+            Thêm
+          </Button>
+        }
+        css={{ padding: '2px 4px 2px' }}
+        value={textInput}
+        onChange={onTextInputChange}
+      ></Textfield>
+
+      <TodoList todoList={todoList} onCheckBtnClick={onCheckBtnClick} />
+    </>
   );
 }
 
